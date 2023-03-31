7b63a5d5b6e309693d32223aef8ce396e98b3185
nirbhay <yadavnirbhay2@gmail.com>
2023-03-31 16:15:19 +0530

    localStorage is added to project to save the details of favourite movies

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ function renderMoviesList(){
         let div  = createElement(movieList[i]);
         movieContainer.appendChild(div);
     }
+
+    // console.log(movieList[2]);
 }
 
 // create the div element inside the movie-conatiner
@@ -53,13 +55,34 @@ function createElement(movie){
     // console.log(movie.original_title);
     const div = document.createElement('div');
     div.className = 'movie-item';
+    let oldMovies=[];
+    if(localStorage.getItem('moviesId')!==null){
+        oldMovies = localStorage.getItem('moviesId').split(',');
+    }
+    let isPresent = false;
+    for(let i=0; i<oldMovies.length; i++){
+        if(oldMovies[i] == movie.id){
+            isPresent = true;
+        }
+    }
 
-    div.innerHTML = `<a href="./moviePage.html?id=${movie.id}"><img src="${TMDB_IMAGE_BASE_URL+movie.poster_path}" alt=""></a>
+    if(!isPresent){
+        div.innerHTML = `<a href="./moviePage.html?id=${movie.id}"><img src="${TMDB_IMAGE_BASE_URL+movie.poster_path}" alt=""></a>
                     <div class="movie-name">${movie.original_title}</div>
                     <div class="rating-and-favourite">
                         <span class="rating">${movie.vote_average}<i class="fa-solid fa-star" style="color: #ffd700;"></i></span>
-                        <span><a href=""><i class="fa-sharp fa-solid fa-heart" style="color: #fcf7f7;"></i></a></span>
+                        <span id="${movie.id}" class="add-to-favourite" onclick="addToFavouriteList('${movie.id}')"><i class="fa-sharp fa-solid fa-heart" style="color: #fcf7f7;"></i></span>
                     </div>`
+    }
+    else{
+        div.innerHTML = `<a href="./moviePage.html?id=${movie.id}"><img src="${TMDB_IMAGE_BASE_URL+movie.poster_path}" alt=""></a>
+                    <div class="movie-name">${movie.original_title}</div>
+                    <div class="rating-and-favourite">
+                        <span class="rating">${movie.vote_average}<i class="fa-solid fa-star" style="color: #ffd700;"></i></span>
+                        <span id="${movie.id}" class="add-to-favourite" onclick="addToFavouriteList('${movie.id}')"><i class="fa-sharp fa-solid fa-heart" style="color: #ff0000;"></i></span>
+                    </div>`
+    }
+    
     return div;
 }
 
@@ -79,3 +102,49 @@ searchMovie.addEventListener('keyup', ()=>{
     }
 
 });
+
+// localStorage.clear();
+// localStorage.setItem('moviesId',[1234, 4567]);
+// const temp = localStorage.getItem('moviesId').split(',');
+// console.log(typeof(temp), temp);
+
+
+function addToFavouriteList(id){
+    if(localStorage.getItem('moviesId')==null){
+        localStorage.setItem('moviesId', [id]);
+        document.getElementById(id).innerHTML = '<i class="fa-sharp fa-solid fa-heart" style="color: #ff0000;"></i>';
+        // console.log('list is null',localStorage.getItem('moviesId'));
+    }
+    else{
+        let oldMoviesId = localStorage.getItem('moviesId').split(',');
+        // console.log(oldMoviesId);
+        let isFound = false;
+        for(let i=0; i<oldMoviesId.length; i++){
+            if(oldMoviesId[i] == id){
+                isFound = true;
+                let removed = oldMoviesId.splice(i,1);
+                // console.log('removed id:', removed);
+                // console.log('movies in list',oldMoviesId);
+                document.getElementById(id).innerHTML='<i class="fa-sharp fa-solid fa-heart" style="color: #fcf7f7;"></i>';
+            }
+        }
+
+        if(!isFound){
+            oldMoviesId.push(id);
+            // console.log('movie not found in list', oldMoviesId);
+            document.getElementById(id).innerHTML = '<i class="fa-sharp fa-solid fa-heart" style="color: #ff0000;"></i>';
+        }
+
+        if(oldMoviesId.length==0){
+            // console.log('oldMovies list length is 0');
+            localStorage.clear();
+        }
+        else{
+            localStorage.setItem('moviesId', oldMoviesId);
+        }
+        // console.log(localStorage.getItem('moviesId'));
+    }
+}
+
+console.log(localStorage.getItem('moviesId'));
+// localStorage.clear();
